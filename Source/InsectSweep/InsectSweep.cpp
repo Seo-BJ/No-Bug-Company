10ba66a142bcc87d135db89b0f54c6cca7d7d9fe
서범준 / BeomJun Seo <88536510+Seo-BJ@users.noreply.github.com>
2024-09-09 01:13:02 +0900

    Add .gitignore file

--- Source/InsectSweep/InsectSweep.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "InsectSweep.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, InsectSweep, "InsectSweep" );
-
-DEFINE_LOG_CATEGORY(LogInsectSweep)
- 
